f2ef201b214e2973b60dd47985d40b0adbce216d
Mike DevProduct Inozemcev <devprodct@gmail.com>
2022-01-23 01:07:10 +0200

    add .

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -6,8 +6,9 @@ module.exports = {
   solidity: '0.8.0',
   networks: {
     ropsten: {
-      url: `https://eth-ropsten.alchemyapi.io/v2/K-arJlr2Za0QdJRppNhfrAVrrc0kfd6m`,
-      accounts: [ 'd08a836a5f14bebb5269c5fcbab394bb763851717735c1149acb4f119c62c9e2' ]
+      url: process.env.URL,
+      accounts: [ process.env.ACCOUNT ]
     }
   }
 }
+
